703664fbc2e4ebc5e0bb671fae5c4ea20ba0a0da
g-eipi10 <95273808+g-eipi10@users.noreply.github.com>
2021-11-30 17:20:55 +0700

    Add files via upload

---
+++ vdown.py
@@ -0,0 +1,24 @@
+import subprocess
+
+print('\n=================================\nDownload YouTube - v1\n=================================')
+
+# Update yt-dlp
+update = f"./yt-dlp.exe -U"
+subprocess.call(['powershell', update])
+
+while True:
+    print('\nPlease paste YouTube link here or press Enter to exit')
+    print('Vui long dan link YouTube vao day hoac nhan Enter de thoat')
+    vid = input('>>> ')
+    if vid == '':
+        break
+    else:
+        vid = vid.replace('"','')
+        vid = vid.split('&')[0]
+        print(f'--- {vid} ---')
+
+        command = f"./yt-dlp.exe -N 32 --remux-video mp4 --ffmpeg-location ./ffmpeg.exe {vid}"
+        subprocess.call(['powershell', command])
+
+        print('\n\n--------------------------\nFinish. Enjoy the video\n--------------------------')
+        # input('Enter to exit...')
